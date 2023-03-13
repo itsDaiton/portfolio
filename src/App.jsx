7b3f1660c5ff4089e2a00621992f28674902ecd2
David Poslušný <david.poslusny@email.cz>
2023-03-13 23:43:10 +0100

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='w-[100px] h-[100px] bg-black flex rounded-2xl justify-center items-center p-5 m-5 text-white text-center'>Hello world.</div>    
+    </div>
+  )
+}
+
+export default App
+
