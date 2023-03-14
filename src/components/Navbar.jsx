0a86bd8780a96833bd8fde5a360211ceef58b74b
David Poslušný <david.poslusny@email.cz>
2023-03-14 13:44:24 +0100

    initial navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { navigation } from '../../utils/data'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar.   
+    <div className='flex justify-between items-center w-full py-5 text-[#020020] font-poppins'>
+      <div className='text-[38px] font-semibold ml-16'>David.</div>
+      <ul className='list-none flex justify-end items-center flex-1'>
+        {navigation.map((link) => (
+          <li
+            key={link.id}
+            className='cursor-pointer text-[20px] mr-16'
+          >
+            <a href={`${link.title}`}>
+              {link.title}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
---
+++ utils/data.js
@@ -0,0 +1,18 @@
+export const navigation = [
+  {
+    id: 1,
+    title: 'About'
+  },
+  {
+    id: 2,
+    title: 'Skills'
+  },
+  {
+    id: 3,
+    title: 'Projects'
+  },
+  {
+    id: 4,
+    title: 'Contact'
+  }
+]
