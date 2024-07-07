e7ccdfe1dc59ee9daa201279ded646dfa16c7703
David Poslušný <david.poslusny@email.cz>
2024-07-07 12:33:05 +0200

    feat: update hero, new icon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
       <div className='flex lg:flex-row flex-col lg:justify-between justify-start items-center w-full py-10'>
         <div className='flex flex-col md:py-52 py-36 justify-start lg:w-fit w-full'>
           <h1 className='font-semibold xl:text-[72px] lg:text-[56px] md:text-[48px] sm:text-[42px] es:text-[36px] text-[32px]'>David Poslušný</h1>
-          <p className='font-normal xl:text-[36px] lg:text-[32px] md:text-[28px] sm:text-[24px] es:text-[20px] text-[16px]'>Applied Informatics Student</p>
+          <p className='font-normal xl:text-[32px] lg:text-[28px] md:text-[24px] sm:text-[20px] es:text-[14px] text-[12px]'>Data Scientist Student | Deep Learning Enthusiast</p>
           <div className='flex es:flex-row flex-col es:items-center items-start es:space-x-5 space-x-0'>
             <motion.a
               className='flex justify-start items-center sm:text-[20px] text-[16px] es:px-10 px-5 es:py-5 py-3 es:my-5 my-3 space-x-2 w-fit 
@@ -62,10 +62,10 @@ const Hero = () => {
           rounded-full bg-black dark:bg-gray-700 absolute sm:-top-28 -top-10 sm:-left-28 -left-10 z-10'/>
           <div>
             <img
-              src='/icon.png'
+              src='/photo.jpg'
               alt='picture'
               className='2xl:w-[450px] 2xl:h-[450px] xl:w-[375px] xl:h-[375px] lg:w-[300px] lg:h-[300px] md:w-[400px] md:h-[400px] sm:w-[300px] sm:h-[300px] w-[200px] h-[200px]
-              object-cover absolute z-20'
+              object-cover absolute z-20 rounded-full object-[center_30%]'
             />
           </div>
         </div>
